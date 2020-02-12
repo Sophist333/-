e00baab9fa7777ba4666364d1e80a05f25b4e5fb
Sophist333 <57650277+Sophist333@users.noreply.github.com>
2020-02-12 20:03:09 +0900

    Create dot_install_4to7.java

---
+++ dot_install_4to7.java
@@ -0,0 +1,75 @@
+public class MyApp{
+	
+	// comment
+	
+	/*
+	 * comment
+	 * comment
+	 */
+	
+	public static void main(String[] args) {
+		//変数
+		//型　変数名
+		
+		//文字
+		//char a='a';//シングルクオーテーション
+		//文字列
+		//String msg;
+		//整数 byte short int long
+		//int x = 10;
+		//long y = 5555555L;
+		//浮動小数点数 float double
+		//double d = 23423.344;
+		//float f = 32.33F;
+		// 論理値
+		//boolean flag = true; //false
+		
+		// \n　改行
+		// \t タブ
+		//msg="Hello Wo\nrld Again\t!";
+		//System.out.println(msg);
+		
+		//演算
+		
+		// + - * / %
+		// ++ --
+		//int i;
+		//i = 10 / 3;
+		//System.out.println(i);//3
+		//i = 10 % 3;
+		//System.out.println(i);//1
+		//x = 5;
+		//x++;
+		//System.out.println(x);//6
+		//x--;
+		//System.out.println(x);//5
+		
+		//x = 5;
+		//x = x + 12;
+		//x += 12;
+		//System.out.println(x);//17
+		
+		//String s;
+		//s = "hello" + "world";
+		//System.out.println(s);
+		
+		//データ型の変換(キャスト)
+		
+		//double d = 52343.231;
+		//int i = d;
+		//System.out.println(i); //error
+		
+		//int i = (int)d;
+		//System.out.println(i); //52343
+		
+		//int i = 10;
+		//double d = i / 4; //2→2.0
+		//System.out.println(d); //2.0
+		
+		//int i = 10;
+		//double d = (double)i / 4;
+		//System.out.println(d); //2.5
+		
+		//Ctrl+/で一括コメントアウト
+	}
+}
