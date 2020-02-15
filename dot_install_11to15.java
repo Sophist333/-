8a681b6417e5badf8a9189ca731af913331153a0
Sophist333 <57650277+Sophist333@users.noreply.github.com>
2020-02-16 08:32:14 +0900

    Create dot_install_11to15.java
    for

---
+++ dot_install_11to15.java
@@ -0,0 +1,77 @@
+public class MyApp{
+	
+	public static void main(String[] args) {
+		// for 
+		// 0-9
+		// break
+		// continue
+//		for(int i = 0; i < 10; i++) {
+//			if (i == 5) {
+//				break; //5以降は表示されない
+//				continue; //5だけ表示されない
+//			} 
+//			System.out.println(i);
+//			}
+
+		
+		//　配列
+		// sale1, sale2,
+		// sales
+		
+//		int[] sales;
+//		sales = new int[3];
+//		
+//		sales[0] = 100;
+//		sales[1] = 200;
+//		sales[2] = 300;
+		
+//		int[] sales;
+//		sales = new int[] {100,200,300};
+		
+//		int[] sales = {100,200,300};
+//		
+//		System.out.println(sales[1]); //200
+//		
+//		sales[1] = 1000;
+//		
+//		System.out.println(sales[1]); //1000
+		
+		// sales.length
+//		int[] sales = {700,400,500};
+		
+//		for (int i = 0; i < sales.length; i++) {
+//			System.out.println(sales[i]);}
+		
+//		// salesからint型のsaleをひとつづつ引っ張ってくる
+//		for (int sale : sales) {
+//			System.out.println(sale);
+//		
+//		}
+		
+		//基本データ型はメモリにデータそのものが入る
+		//参照型はそのデータが入っている番地の情報が入る
+		
+		// 基本データ型
+//		int x = 10;
+//		int y = x;
+//		y = 5;
+//		System.out.println(x);//10
+//		System.out.println(y);//5
+		
+		// 参照型
+//		int[] a = {3,5,7};
+//		int[] b = a; //bにはメモリ領域の番地が入る→bを変更すればaも変更される
+//		b[1] = 8;
+//		System.out.println(a[1]);//8
+//		System.out.println(b[1]);//8	
+		
+//		String s = "hello";
+//		String t = s;
+//		t = "world";
+//		System.out.println(s);//hello
+//		System.out.println(t);//world
+		
+		//文字列を書き換えることは不可能→別の番地にデータが格納される
+		
+	}
+}	
