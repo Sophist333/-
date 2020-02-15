c1869f07032341b9fc7ae43c3ddfd6a1b356b223
Sophist333 <57650277+Sophist333@users.noreply.github.com>
2020-02-16 08:47:47 +0900

    Create dot_install_16to17.java
    method

---
+++ dot_install_16to17.java
@@ -0,0 +1,38 @@
+public class MyApp{
+	//method
+	
+	//返り値のデータ型を宣言する
+//	public static String sayHi(String name) {
+//		// System.out.println("Hi!");
+//		// System.out.println("Hi! " + name);
+//		return "Hi! " + name;
+//	}
+	
+	// メソッドのオーバーロード
+	// 引数の数によって挙動を変更
+	
+//	public static void sayHi(String name) {
+//		int x = 10;
+//		System.out.println("Hi! " + name);
+//	}
+//	
+//	public static void sayHi() {
+//		System.out.println("Hi! Nobody!");
+//	}
+//	
+//	
+//	public static void main(String[] args) {
+//		// sayHi()
+////		sayHi("Tom");
+////		sayHi("Bob");
+////		String msg = sayHi("Steve");
+////		System.out.println(msg);
+//		
+//		//オーバーロード
+//		sayHi();
+//		sayHi("Steve");//引数
+////		System.out.println(name);
+////		System.out.println(x);
+
+	}	
+}
