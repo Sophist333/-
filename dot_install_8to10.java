7eafaa123514abbafdac343bc010c3b8cf4d970c
Sophist333 <57650277+Sophist333@users.noreply.github.com>
2020-02-16 08:02:52 +0900

    Create dot_install_8to10.java

---
+++ dot_install_8to10.java
@@ -0,0 +1,52 @@
+public class MyApp{
+	
+	public static void main(String[] args) {
+//		//if
+//		//> >= < <= == !=
+//		//&& \\ !
+//
+//		int score = 85; //Great
+//		if (score > 80) {
+//			System.out.println("Great!");
+//		} else if (score > 60) {
+//			System.out.println("Good");
+//		} else {
+//			System.out.println("so so ... !");
+//		}
+		
+//		String msg = score > 80 ? "Great!" : "so so ... !";
+//		System.out.println(msg);
+
+//		// switch
+//		String signal = "green";
+//		switch (signal) {
+//			case "red":
+//				System.out.println("stop!");
+//				break; //breakを忘れない
+//			case "blue":
+//			case "green":
+//				System.out.println("go!");
+//				break;
+//			case "yellow":
+//				System.out.println("caution!");
+//				break;
+//			default:
+//				System.out.println("wrong signal!");
+//				break;
+//		}
+		
+		// while 
+		// 0 - 9
+		
+//		int i = 100;
+//		while (i < 10) {
+//			System.out.println(i);
+//			i++; //一度もiが表示されない
+//		}
+//		do {
+//			System.out.println(i);
+//			i++;
+//		} while (i < 10);　//100が一度表示される
+		//　後判定型反復
+	}
+}
